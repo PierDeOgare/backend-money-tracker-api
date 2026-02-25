75326d97dfaee5bc92ebffc0048afd379f008ca0
PierDeOgare <157198389+PierDeOgare@users.noreply.github.com>
2026-02-25 14:45:06 +0300

    implemented create, store and show user profile with total balance for all wallets belonging to respective user

--- money-tracker-api/app/Http/Controllers/UserController.php
+++ money-tracker-api/app/Http/Controllers/UserController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    /**CREATE AND STORE USER */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+        ]);
+        return User::create($request->all());
+    }
+
+    /**SHOW USER PROFILE */
+    public function show($id)
+    {
+        $user = User::with('wallets.transactions')->findOrFail($id);
+        $totalBalance = $user->wallets->sum->balance;
+
+        return response()->json([
+            'user' => $user,
+            'total_balance' => $totalBalance
+        ]);
+    }
 }
