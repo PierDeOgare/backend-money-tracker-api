da944eb09ea0d4485d011a4bcb6c6781a9fc3f8b
PierDeOgare <157198389+PierDeOgare@users.noreply.github.com>
2026-02-25 13:38:00 +0300

    impemented and enforced transaction creation with validity rules

--- money-tracker-api/app/Http/Controllers/TransactionController.php
+++ money-tracker-api/app/Http/Controllers/TransactionController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class TransactionController extends Controller
 {
-    //
+    /**STORE TRANSACTION INFO RESPECTIVE OF THE WALLET  */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'wallet_id' => 'required|exists:wallets,id',
+            'type' => 'required|in:income,expense',
+            'amount' => 'required|numeric|min:0.01',
+            'description' => 'nullable|string'
+        ]);
+        $transaction = Transaction::create($request->all());
+        return response()->json($transaction, 201);
+    }
 }
